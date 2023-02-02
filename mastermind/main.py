d6c6f933eeed09efaa01e7163f392186b6c225f5
Martin <74346891+RealDiLink@users.noreply.github.com>
2023-02-02 22:50:42 +0100

    In line colors functions
    Added an error message box when trying to put more than 4 colors on the same line. Added an error message box when trying to validate without having 4 colors in the same line.
    Need to add color verification + victory message.

--- mastermind/main.py
+++ mastermind/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 import os
 
 window = Tk()
@@ -40,54 +41,80 @@ nb_color_in_line = 0
 
 list_actual_color = []
 
+color_check = []
+
+color_suit = ["vert", "bleu", "orange", "violet"]
+
 
 def set_color(color):
     global nb_color_in_line
+    global nb_tentative
+    if (color == "valider"):
+        if (nb_color_in_line != 4):
+            messagebox.showerror("Erreur de jeu", "4 couleurs sont nécessaires pour valider la ligne !\n"
+                                                  "Vous devez choisir 4 couleurs.\n")
+            return False
+        nb_tentative += 1
+        nb_color_in_line = 0
+        list_actual_color.clear()
+        if (color_check == color_suit):
+            print("GAGNE")
+        color_check.clear()
+        return
     if (color == "del"):
         for element in list_actual_color:
             element.after(5, element.destroy())
         list_actual_color.clear()
+        color_check.clear()
         nb_color_in_line = 0
-        print(nb_color_in_line)
         return
-    if(nb_color_in_line == 4):
-        print("TROP DE COULEURS CHOISIT FAUT FAIRE UN MESSAGE DERREUR")
+    if (nb_color_in_line == 4):
+        messagebox.showerror("Erreur de jeu", "Trop de couleur choisit !\nVous ne pouvez choisir que 4 couleurs par "
+                                              "ligne maximum.\nPour changer la combinaison appuyer sur le boutton "
+                                              "SUPPRIMER ")
         return False
     if (color == "vert"):
         actual_color = Label(window, image=img_vert, bg="red", highlightthickness=0, bd=0)
-        actual_color.place(x=0, y=71 * nb_tentative)
+        actual_color.place(x=20 + 85 * nb_color_in_line, y=2 + 71 * nb_tentative)
         list_actual_color.append(actual_color)
+        color_check.append("vert")
     if (color == "bleu"):
         actual_color = Label(window, image=img_bleu, bg="red", highlightthickness=0, bd=0)
-        actual_color.place(x=0, y=71 * nb_tentative)
+        actual_color.place(x=20 + 85 * nb_color_in_line, y=2 + 71 * nb_tentative)
         list_actual_color.append(actual_color)
+        color_check.append("bleu")
     if (color == "orange"):
         actual_color = Label(window, image=img_orange, bg="red", highlightthickness=0, bd=0)
-        actual_color.place(x=0, y=71 * nb_tentative)
+        actual_color.place(x=20 + 85 * nb_color_in_line, y=2 + 71 * nb_tentative)
         list_actual_color.append(actual_color)
+        color_check.append("orange")
     if (color == "gris"):
         actual_color = Label(window, image=img_gris, bg="red", highlightthickness=0, bd=0)
-        actual_color.place(x=0, y=71 * nb_tentative)
+        actual_color.place(x=20 + 85 * nb_color_in_line, y=2 + 71 * nb_tentative)
         list_actual_color.append(actual_color)
+        color_check.append("gris")
     if (color == "rouge"):
         actual_color = Label(window, image=img_rouge, bg="red", highlightthickness=0, bd=0)
-        actual_color.place(x=0, y=71 * nb_tentative)
+        actual_color.place(x=20 + 85 * nb_color_in_line, y=2 + 71 * nb_tentative)
         list_actual_color.append(actual_color)
+        color_check.append("rouge")
     if (color == "violet"):
         actual_color = Label(window, image=img_violet, bg="red", highlightthickness=0, bd=0)
-        actual_color.place(x=0, y=71 * nb_tentative)
+        actual_color.place(x=20 + 85 * nb_color_in_line, y=2 + 71 * nb_tentative)
         list_actual_color.append(actual_color)
+        color_check.append("violet")
     if (color == "blanc"):
         actual_color = Label(window, image=img_blanc, bg="red", highlightthickness=0, bd=0)
-        actual_color.place(x=0, y=71 * nb_tentative)
+        actual_color.place(x=20 + 85 * nb_color_in_line, y=2 + 71 * nb_tentative)
         list_actual_color.append(actual_color)
+        color_check.append("blanc")
     if (color == "jaune"):
         actual_color = Label(window, image=img_jaune, bg="red", highlightthickness=0, bd=0)
-        actual_color.place(x=0, y=71 * nb_tentative)
+        actual_color.place(x=20 + 85 * nb_color_in_line, y=2 + 71 * nb_tentative)
         list_actual_color.append(actual_color)
+        color_check.append("jaune")
     nb_color_in_line += 1
-
-    print(nb_color_in_line)
+    return True
 
 
 img_vert = PhotoImage(file=ABSOLUTE_PATH + "vert.png")
@@ -99,6 +126,7 @@ img_violet = PhotoImage(file=ABSOLUTE_PATH + "violet.png")
 img_blanc = PhotoImage(file=ABSOLUTE_PATH + "blanc.png")
 img_jaune = PhotoImage(file=ABSOLUTE_PATH + "jaune.png")
 img_del = PhotoImage(file=ABSOLUTE_PATH + "button_delet.png")
+img_valider = PhotoImage(file=ABSOLUTE_PATH + "valider.png")
 
 button_vert = Button(window, image=img_vert, bg="#b97a57", activebackground="#b97a57", highlightthickness=0, bd=0,
                      command=lambda: set_color("vert"))
@@ -118,6 +146,8 @@ button_jaune = Button(window, image=img_jaune, bg="#b97a57", activebackground="#
                       command=lambda: set_color("jaune"))
 button_del = Button(window, image=img_del, bg="#b97a57", activebackground="#b97a57", highlightthickness=0, bd=0,
                     command=lambda: set_color("del"))
+button_valider = Button(window, image=img_valider, bg="#b97a57", activebackground="#b97a57", highlightthickness=0, bd=0,
+                        command=lambda: set_color("valider"))
 
 button_vert.place(x=606, y=0)
 button_bleu.place(x=606, y=71)
@@ -128,5 +158,6 @@ button_violet.place(x=606, y=71 * 5)
 button_jaune.place(x=606, y=71 * 6)
 button_blanc.place(x=606, y=71 * 7)
 button_del.place(x=606, y=71 * 8)
+button_valider.place(x=606, y=71 * 9)
 
 window.mainloop()
