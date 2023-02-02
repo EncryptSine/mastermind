2642de424dd9a453fabc01293009f0ec469ac354
DiLink <74346891+RealDiLink@users.noreply.github.com>
2023-02-02 12:58:25 +0100

    Salut j'avance trop pas

--- mastermind/main.py
+++ mastermind/main.py
@@ -1,9 +1,11 @@
 import tkinter as tk
+import os
 
 window = tk.Tk()
 
 WINDOW_HEIGHT = 920
 WINDOW_WIDTH = 720
+ABSOLUTE_PATH = fr"{os.path.dirname(__file__)}/../img/"
 
 # get the screen dimension
 screen_width = window.winfo_screenwidth()
@@ -16,25 +18,45 @@ center_y = int(screen_height / 2 - WINDOW_HEIGHT / 2)
 window.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}+{center_x}+{center_y}")
 window.title("Mastermind")
 
-photo = tk.PhotoImage(file="P:\\NSI\\python\\mastermind\\mastermind\\img\\logo.png")
+photo = tk.PhotoImage(file=ABSOLUTE_PATH + "logo.png")
 window.iconphoto(False, photo)
 
 window['background'] = '#f0f0f0'
 
 window.resizable(False, False)
 
-absolute_path = "P:\\NSI\\python\\mastermind\\mastermind\\img\\"
 
-img_path = [absolute_path + 'vert.png', absolute_path + 'bleu.png',absolute_path + 'orange.png',absolute_path + 'gris.png',absolute_path + 'rouge.png',absolute_path + 'violet.png', absolute_path + 'blanc.png', absolute_path + 'jaune.png', absolute_path + "button_delet.png"]
-image_list = [tk.PhotoImage(file=x) for x in img_path]
+def set_color(button):
+    print(button)
 
-i = 0
-for item in image_list: # Number of rows
-    for j in range(1): # Number of columns
-        lbl = tk.Label(window,image=image_list[i])
-        lbl.config
-        lbl.grid(row=i,column=j)
-    i += 1
 
+button_dico = {"vert": "button0",
+               "bleu": "button1",
+               "orange": "button2",
+               "gris": "button3",
+               "rouge": "button4",
+               "violet": "button5",
+               "blanc": "button6",
+               "jaune": "button7"}
+
+
+GO FAIRE TOUT LES BUTTONS UN A UN CAR C GALERE JE SUIS PAS UN PRO DE LA POO PYTHON
+
+image_list = [tk.PhotoImage(file=ABSOLUTE_PATH + x + ".png") for x in button_dico]
+
+high = 0
+j = 0
+# GROS MANQUE D'OPTIMISATION MAIS CA MARCHE
+for item in image_list:
+    for j in range(1):  # Number of columns
+        button = tk.Button(window, image=item)
+        button.place(x=650, y=high)
+        for key in button_dico:
+            if button_dico[key] == "button" + str(j):
+                button_dico[key] = button
+            j += 1
+    high += 70
+
+print(button_dico)
 
 window.mainloop()
